687bb68787dc8b743b9bfcc16a2ee7d4fc391c51
Ryan <omullan.ryan@gmail.com>
2017-11-23 12:10:34 -0200

    Add css to app

--- src/App.js
+++ src/App.js
@@ -55,14 +55,16 @@ class App extends Component {
         const { searchTerm, list } = this.state;
 
         return (
-            <div className="App">
+            <div className="page">
                 <h2>{helloWorld}</h2>
-                <Search
-                    value={searchTerm}
-                    onChange={this.onSearchChange}
-                >
-                    Search:
-                </Search>
+                <div className="interactions">
+                    <Search
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                    >
+                        Search:
+                    </Search>
+                </div>
                 <hr/>
                 <Table
                     list={list}
@@ -85,18 +87,28 @@ const Search = ({ value, onChange, children }) => {
 }
 
 const Table = ({ list, pattern, onDismiss }) => {
+
+    const largeColumn = { width: '40%',
+    };
+    const midColumn = { width: '30%',
+    };
+    const smallColumn = { width: '10%',
+    };
+
     return (
-        <div>
+        <div className="table">
             {list.filter(isSearched(pattern)).map(item =>
-                <div key={item.objectID}>
-                    <span>
+                <div key={item.objectID} className="table-row">
+                    <span style={largeColumn}>
                         <a href={item.url}>{item.title}</a>
                     </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                    <span>
-                        <Button onClick={() => onDismiss(item.objectID)}>
+                    <span style={midColumn}>{item.author}</span>
+                    <span style={smallColumn}>{item.num_comments}</span>
+                    <span style={smallColumn}>{item.points}</span>
+                    <span style={smallColumn}>
+                        <Button onClick={() => onDismiss(item.objectID)}
+                                className="button-inline"
+                        >
                             Dismiss
                         </Button>
                     </span>
